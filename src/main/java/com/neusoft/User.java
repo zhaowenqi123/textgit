0e0480e3a30cfe9ab31c7a6b11f9eba67afd5a76
‘zhaowenqi123’ <zhaowenqi@qq.com>
2019-03-05 11:54:01 +0800

    修改了

---
+++ src/main/java/com/neusoft/User.java
@@ -0,0 +1,10 @@
+package com.neusoft;
+
+public class User {
+    public static void main( String[] args )
+    {
+        System.out.println( "张三改的" );
+        System.out.println( "李四改的" );
+    }
+
+}
